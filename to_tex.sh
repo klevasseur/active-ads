37e8e49e63af7bd819179cd2dcafd4525a041ef7
Ken Levasseur <klevasseur@mac.com>
2020-01-31 09:22:02 -0500

    initial commit

---
+++ to_tex.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xsltproc --xinclude  --stringparam exercise.divisional.solution "no" --stringparam exercise.divisional.answer  "no"  ../../mathbook/xsl/mathbook-latex.xsl src/active-ads.xml > active-ads.tex
